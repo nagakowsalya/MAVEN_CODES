3c7b53964f1b07d68c1cb29b9ec9364fe03729a7
nagakowsalya <nagakowsalya@gmail.com>
2021-09-08 23:12:20 +0100

    Committing the new driver changes

--- src/test/java/utilities/TestCase.java
+++ src/test/java/utilities/TestCase.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -27,5 +28,10 @@ public class TestCase {
 		driver.get("gmail.com");
 		driver.findElement(By.id("Email")).sendKeys("nagakowsalya");
 		
+	}
+	@AfterMethod
+	public void tearDown()
+	{
+		driver.quit();
 	}
 }
